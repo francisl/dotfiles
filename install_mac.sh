35de4a7c61be7601ebfc9fbe15476b6c36aefefa
Frank Lavoie <lav.francis@gmail.com>
2014-10-08 00:32:29 -0400

    copy config.fish plus installation

--- install_mac.sh
+++ install_mac.sh
@@ -5,3 +5,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install fish
 
 chsh -s /usr/local/bin/fish
+
+git clone git://github.com/bpinto/oh-my-fish.git ~/.oh-my-fish
+
+wget https://raw.githubusercontent.com/francisl/dotfiles/master/config.fish > ~/.config/fish/config.fish

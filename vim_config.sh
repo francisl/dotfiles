95bfc6fd6eaeac873ea9e9a68b0b2d5b4d567d61
Francis Lavoie <f.lavoie@onf.ca>
2014-12-29 11:49:01 -0500

    added superuser for vim pip dependencies

--- vim_config.sh
+++ vim_config.sh
@@ -12,7 +12,7 @@ git submodule foreach git submodule init
 git submodule foreach git submodule update
 
 # install python depencies
-pip install rope
-pip install ropemode
-pip install ropevim
-pip install pep8
+sudo pip install rope
+sudo pip install ropemode
+sudo pip install ropevim
+sudo pip install pep8

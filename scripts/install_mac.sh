dcddc6071cd56718afdc1a1eaea3d22a0e83e53c
Francis Lavoie <francisl@users.noreply.github.com>
2015-06-15 11:50:18 -0400

    Update install_mac.sh

--- scripts/install_mac.sh
+++ scripts/install_mac.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
 ## ANDROID
-if [ ! -e ~/opt/android-sdk ]; then
-	cd ~/opt
-    wget http://dl.google.com/android/android-sdk_r23.0.2-macosx.zip
-    unzip android-sdk_r23.0.2-macosx.zip
-    rm android-sdk_r23.0.2-macosx.zip
-    mv android-sdk-macosx android-sdk
-fi
+# if [ ! -e ~/opt/android-sdk ]; then
+# 	cd ~/opt
+#     wget http://dl.google.com/android/android-sdk_r23.0.2-macosx.zip
+#     unzip android-sdk_r23.0.2-macosx.zip
+#     rm android-sdk_r23.0.2-macosx.zip
+#     mv android-sdk-macosx android-sdk
+# fi
 
-if [ ! -e ~/opt/android-ndk ]; then
-    wget http://dl.google.com/android/ndk/android-ndk64-r10b-darwin-x86_64.tar.bz2
-    tar -xjf android-ndk64-r10b-darwin-x86_64.tar.bz2
-    rm android-ndk64-r10b-darwin-x86_64.tar.bz2
-    ln -s android-ndk-r10b android-ndk
-fi
+# if [ ! -e ~/opt/android-ndk ]; then
+#     wget http://dl.google.com/android/ndk/android-ndk64-r10b-darwin-x86_64.tar.bz2
+#     tar -xjf android-ndk64-r10b-darwin-x86_64.tar.bz2
+#     rm android-ndk64-r10b-darwin-x86_64.tar.bz2
+#     ln -s android-ndk-r10b android-ndk
+# fi
 
-if [ ! `which javac` ]; then
-    cd ~/Downloads
-    curl http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-macosx-x64.dmg
-    open jdk-8u25-macosx-x64.dmg
-fi
+# if [ ! `which javac` ]; then
+#     cd ~/Downloads
+#     curl http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-macosx-x64.dmg
+#     open jdk-8u25-macosx-x64.dmg
+# fi
 
-## Eclipse
-if [ ! -e ~/opt/eclipse ]; then
-	cd ~/opt
-	wget http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz
-    tar -zxf eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz
-fi
+# ## Eclipse
+# if [ ! -e ~/opt/eclipse ]; then
+# 	cd ~/opt
+# 	wget http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz
+#     tar -zxf eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz
+# fi
 
 
 ## BREW
@@ -46,12 +46,16 @@ if [ ! `which fish` ]; then
 fi
 #chsh -s /usr/local/bin/fish
 
+if [ ! `which node` ]; then
+    brew install node
+fi
+
 ## VIM OSX
 brew install vim --override-system-vim --without-python
-if [ ! `which nvim` ]; then
-    brew tap neovim/homebrew-neovim
-    brew install --HEAD neovim
-fi 
+# if [ ! `which nvim` ]; then
+#     brew tap neovim/homebrew-neovim
+#     brew install --HEAD neovim
+# fi 
 
 ## EMACS
 brew install emacs --with-cocoa

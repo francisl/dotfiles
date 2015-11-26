95281fddd0c6df075aa92f3b3178f0cf36d7892a
Francis Lavoie <lav.francis@gmail.com>
2015-11-25 20:39:47 -0500

    update zsh script

---
+++ plugins/zsh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Installing ZSH"
+
+if [ ! -z ~/.zprezto ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
+fi
+
+zsh ~/dotfiles/plugins/zsh_config.zsh
+
+ln -sf ~/dotfiles/config/zpreztorc .zpreztorc

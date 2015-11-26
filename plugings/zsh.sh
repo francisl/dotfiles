a519613cc06709934b2a3ee4515414f0777a136b
Francis Lavoie <lav.francis@gmail.com>
2015-11-25 20:27:31 -0500

    update zsh linux

--- plugings/zsh.sh
+++ plugings/zsh.sh
@@ -1,11 +1,16 @@
-## ZSH
-echo "Installing ZSH"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
+#!/usr/bin/env bash
 
-zsh
+echo "Installing ZSH"
+
+if [ ! -z ~/.zprezto ]; then
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
+fi
+
+zsh << EOF
 setopt EXTENDED_GLOB
 for rcfile in "${HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${HOME}/.${rcfile:t}"
 done
+EOF
 
-ln -sf ~/dotfiles/zpreztorc .zpreztorc
+ln -sf ~/dotfiles/config/zpreztorc .zpreztorc

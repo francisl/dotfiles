b81050c0bb90924ac94aee67072ec04d97fe227f
Francis <lav.francis+github@gmail.com>
2016-07-10 01:36:31 -0400

    reused code between darwin and linux

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,19 +9,14 @@ cd ~/opt
 ## set environment for bash/ZSH
 if [[ "$platform" == 'Linux' ]]; then
     ~/dotfiles/scripts/install_linux.sh
-    if [ -e ~/.bashrc -a ! -e ~/.local_profile ];then
-        echo >> ~/.bashrc
-        echo . ~/.customrc >> ~/.bashrc
-    fi
-	  ln -sf ~/dotfiles/config/customrc ~/.customrc
-
 elif [[ "$platform" == 'Darwin' ]]; then
     ~/dotfiles/scripts/install_mac.sh
-    if [ -e ~/.profile -a ! -e ~/.local_profile ];then
-      echo >> ~/.bashrc
-      echo . ~/.customrc >> ~/.bashrc
-    fi
-	  ln -sf ~/dotfiles/config/customrc ~/.customrc
+fi
+
+if [ ! -e ~/.local_profile ];then
+  echo >> ~/.bashrc
+  echo . ~/.customrc >> ~/.bashrc
+  ln -sf ~/dotfiles/config/customrc ~/.customrc
 fi
 
 ## ZSH

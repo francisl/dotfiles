5863e62e2af3acf9b0889c1e72f43e48d1341461
Francis Lavoie <francis.lavoie@onespan.com>
2018-11-20 08:45:32 -0500

    fix zsh

--- plugins/zsh.sh
+++ plugins/zsh.sh
@@ -11,3 +11,11 @@ zsh ~/dotfiles/plugins/zsh_config.zsh
 ln -sf ~/dotfiles/config/zpreztorc .zpreztorc
 
 chsh -s $(which zsh)
+
+cat > file.tmp <<'endconfig'
+. /Users/lavoifr1/dotfiles/config/customrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+endconfig

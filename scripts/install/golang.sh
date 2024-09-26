3afe26c997cbc6db50e6abec4cecb5e78cc23a72
Francis Lavoie <lav.francis@gmail.com>
2024-09-26 16:07:01 -0400

    Create golang.sh
    add script to install go lang

---
+++ scripts/install/golang.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+echo -n "Do you want to install go? (y/n): " 
+read install_go
+
+# Check the user's response
+if [[ "$install_go" == "y" || "$install_go" == "Y" ]]; then
+    echo "Installing ZSH"
+    cd ~/Downloads
+    wget https://go.dev/dl/go1.23.1.darwin-arm64.tar.gz
+    sudo tar -C /usr/local -xzf go1.23.1.darwin-arm64.tar.gz
+    if ! grep -qF 'export PATH=$PATH:/usr/local/go/bin' ~/.zshrc; then
+      echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+    fi
+else
+    echo "Not Install: go"
+fi

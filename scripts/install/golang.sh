447dce5ac0d0833c15fc40341c5a8355d31061f3
Francis Lavoie <lav.francis@gmail.com>
2024-09-26 16:10:44 -0400

    Update golang.sh
    switch go lang to arm or amd arch

--- scripts/install/golang.sh
+++ scripts/install/golang.sh
@@ -3,12 +3,21 @@
 echo -n "Do you want to install go? (y/n): " 
 read install_go
 
+# Check the architecture
+ARCH=$(uname -m)
+
 # Check the user's response
 if [[ "$install_go" == "y" || "$install_go" == "Y" ]]; then
     echo "Installing golang"
     cd ~/Downloads
-    wget https://go.dev/dl/go1.23.1.darwin-arm64.tar.gz
-    sudo tar -C /usr/local -xzf go1.23.1.darwin-arm64.tar.gz
+    if [ "$ARCH" = "arm64" ]; then
+        wget https://go.dev/dl/go1.23.1.darwin-arm64.tar.gz
+        sudo tar -C /usr/local -xzf go1.23.1.darwin-arm64.tar.gz
+    else
+        wget https://go.dev/dl/go1.23.1.darwin-amd64.tar.gz
+        sudo tar -C /usr/local -xzf go1.23.1.darwin-amd64.tar.gz
+    fi
+    
     if ! grep -qF 'export PATH=$PATH:/usr/local/go/bin' ~/.zshrc; then
       echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
     fi
